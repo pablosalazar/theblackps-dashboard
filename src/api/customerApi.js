b90ae8da1fb81584443282ceedc045962c30c1c2
Julio César Franco <jfranc38@eafit.edu.co>
2020-01-27 23:17:00 -0500

    fixed axios call in customers

--- src/api/customerApi.js
+++ src/api/customerApi.js
@@ -2,16 +2,20 @@ import axios from 'axios';
 import { BASE_URI, API_URL } from '../constants/defaultValues';
 import FormData from 'form-data'
 
-const axiosInstance = axios.create({
-  baseURL: API_URL + BASE_URI,
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`,
-  }
-});
+function getAxiosIntance() {
+  const axiosInstance = axios.create({
+    baseURL: API_URL + BASE_URI,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    }
+  });
+
+  return axiosInstance;
+}
 
 export async function getCustomers(pageSize, page, orderBy, search) {
   try {
-    const data  = await axiosInstance.get(`/customers?pageSize=${pageSize}
+    const data  = await getAxiosIntance().get(`/customers?pageSize=${pageSize}
         &page=${page}
         &orderBy=${orderBy}
         &search=${search}`
@@ -25,7 +29,7 @@ export async function getCustomers(pageSize, page, orderBy, search) {
 
 export async function getCustomer(id) {
   try {
-    const data  = await axiosInstance.get(`/customers/${id}`);
+    const data  = await getAxiosIntance().get(`/customers/${id}`);
     return data.data;
   } catch (error) {
     throw error.response ? error.response.data.error : error.message;
@@ -35,7 +39,7 @@ export async function getCustomer(id) {
 
 export async function createCustomer(customer) {
   try {
-    const data  = await axiosInstance.post('/customers', getFormData(customer));
+    const data  = await getAxiosIntance().post('/customers', getFormData(customer));
     return data.data;
   } catch (error) {
     throw error.response ? error.response.data.error : error.message;
@@ -44,7 +48,7 @@ export async function createCustomer(customer) {
 
 export async function updateCustomer(id, customer) {
   try {
-    const data  = await axiosInstance.post(`/customers/${id}`, getFormData(customer, 'PUT'));
+    const data  = await getAxiosIntance().post(`/customers/${id}`, getFormData(customer, 'PUT'));
     return data.data;
   } catch (error) {
     throw error.response ? error.response.data.error : error.message;
@@ -53,7 +57,7 @@ export async function updateCustomer(id, customer) {
 
 export async function deleteCustomer(id) {
   try {
-    const data  = await axiosInstance.delete(`/customers/${id}`);
+    const data  = await getAxiosIntance().delete(`/customers/${id}`);
     return data.data;
   } catch (error) {
     throw error.response ? error.response.data.error : error.message;
